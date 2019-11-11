d5e1798c6531abd00061c60b4a97717744e95660
Manuel Gauto <mgauto@mgenterprises.org>
2019-11-11 10:01:22 -0500

    Start websocket support.

---
+++ SwiftLSPClient/JSONRPC/WebSocketDataTransport.swift
@@ -0,0 +1,22 @@
+//
+// Created by Manuel Gauto on 11/11/19.
+// Copyright (c) 2019 Chime Systems. All rights reserved.
+//
+
+import Foundation
+
+public class WebSocketDataTransport: DataTransport {
+    public func write(_ data: Data) {
+        <#code#>
+    }
+
+    public func setReaderHandler(_ handler: @escaping Self.ReadHandler) {
+        <#code#>
+    }
+
+    public func close() {
+        <#code#>
+    }
+
+
+}
