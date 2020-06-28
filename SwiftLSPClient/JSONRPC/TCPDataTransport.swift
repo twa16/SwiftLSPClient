94884d1a73960dd5e73adb7dd79a513fe31aa5c8
Manuel Gauto <mgauto@mgenterprises.org>
2020-06-27 22:06:03 -0400

    Fix up TCPTransport

--- SwiftLSPClient/JSONRPC/TCPDataTransport.swift
+++ SwiftLSPClient/JSONRPC/TCPDataTransport.swift
@@ -9,14 +9,14 @@ import Foundation
 import SwiftSocket
 
 class TCPDataTransport: DataTransport {
-    private host: String
-    private port: Int
-    private socket: TCPClient
-    private readHandler: ReadHandler
+    private var host: String
+    private var port: Int
+    private var socket: TCPClient
+    private var readHandler: ReadHandler
     
-    private shouldRun: Bool
+    private var shouldRun: Bool
     
-    public init(host: String, port: Int) {
+    public init(host: String, port: Int32) {
         self.host = host
         self.port = port
         
@@ -34,14 +34,17 @@ class TCPDataTransport: DataTransport {
     }
     
     func setReaderHandler(_ handler: @escaping ReadHandler) {
-        self.readHandler = readHandler
+        self.readHandler = handler
     }
 
     func checkForData() {
         while self.shouldRun {
             let bytesAvailable = self.socket.bytesAvailable()
-            if bytesAvailabl != nil && bytesAvailable() != 0 {
-                self.readHandler(bytesAvailable)
+            if bytesAvailable != nil && bytesAvailable != 0 {
+                let bytes = self.socket.read(Int(bytesAvailable!))
+                if bytes != nil {
+                    self.readHandler(Data(bytes!))
+                }
             }
         }
     }
